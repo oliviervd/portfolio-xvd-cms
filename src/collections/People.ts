f593dcdae2fefc5e81258cff2014e54d1ae2cff0
oliviervd <olliviervandhuynslager@gmail.com>
2023-09-24 11:02:40 +0200

    update people

--- src/collections/People.ts
+++ src/collections/People.ts
@@ -4,25 +4,16 @@ const People: CollectionConfig = {
     slug: "people",
     fields: [
         {
-            name: "company",
-            defaultValue: "NO_COMPANY",
-            label: "company",
-            type: "text",
-        },
-        {
             name: "firstName",
             label: "first name",
             type: "text",
+            required: true
         },
         {
             name: "lastName",
             label: "last name",
             type: "text",
-        },
-        {
-            name: "website",
-            label: "website",
-            type: "text"
+            required: true
         }
     ]
 }
