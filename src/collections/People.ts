50bfc322679ebf306c6cdd149cef4c7122e77c3a
oliviervd <olliviervandhuynslager@gmail.com>
2023-09-24 09:13:21 +0200

    update people

--- src/collections/People.ts
+++ src/collections/People.ts
@@ -3,20 +3,29 @@ import {CollectionConfig} from "payload/types";
 const People: CollectionConfig = {
     slug: "people",
     admin: {
-        useAsTitle: "firstName" + " " + "lastName"
+        useAsTitle: "company"
     },
     fields: [
+        {
+            name: "company",
+            label: "company",
+            type: "text",
+            required: true,
+            admin: {
+                description: "company this person works for"
+            }
+        },
         {
             name: "firstName",
             label: "first name",
             type: "text",
-            required: true
+            required: false
         },
         {
             name: "lastName",
             label: "last name",
             type: "text",
-            required: true
+            required: false
         }
     ]
 }
